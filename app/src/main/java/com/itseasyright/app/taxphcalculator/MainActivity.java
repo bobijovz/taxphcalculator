1c06aea7c870f0e54804477b1ab3559db7157fed
June Jovi Granados <jovi@tagcash.com>
2016-09-07 17:52:32 +0800

    ui update

--- app/src/main/java/com/itseasyright/app/taxphcalculator/MainActivity.java
+++ app/src/main/java/com/itseasyright/app/taxphcalculator/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.itseasyright.app.taxphcalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private ActivityMainBinding binder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         binder.btnCalculate.setOnClickListener(this);
         binder.btnReset.setOnClickListener(this);
+        binder.headerBasic.setOnClickListener(this);
+        binder.headerMisc.setOnClickListener(this);
+        binder.headerAllowance.setOnClickListener(this);
+        collapse(binder.contentMisc);
+        collapse(binder.contentAllowance);
+        expand(binder.contentBasic);
     }
 
     @Override
@@ -29,7 +39,78 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.btn_reset:
 
                 break;
+            case R.id.header_basic:
+                //binder.headerBasic.setCompoundDrawables(null,null,getResources().getDrawable(R.drawable.ic_keyboard_arrow_up_black_24dp),null);
+                collapse(binder.contentMisc);
+                collapse(binder.contentAllowance);
+                expand(binder.contentBasic);
+                break;
+            case R.id.header_misc:
+                expand(binder.contentMisc);
+                collapse(binder.contentAllowance);
+                collapse(binder.contentBasic);
+                break;
+            case R.id.header_allowance:
+                collapse(binder.contentMisc);
+                expand(binder.contentAllowance);
+                collapse(binder.contentBasic);
+                break;
         }
+    }
+
+
+    public static void expand(final View v) {
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? LinearLayout.LayoutParams.WRAP_CONTENT
+                        : (int)(targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if(interpolatedTime == 1){
+                    v.setVisibility(View.GONE);
+                }else{
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
     }
 
 }
