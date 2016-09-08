e8e319c68c24175ec156fc187685eb6c331e580b
Nico Villegas <john@tagcash.com>
2016-09-08 13:37:24 +0800

    Merged

--- app/src/main/java/com/itseasyright/app/taxphcalculator/SyncDataActivity.java
+++ app/src/main/java/com/itseasyright/app/taxphcalculator/SyncDataActivity.java
@@ -4,27 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.itseasyright.app.taxphcalculator.Entities.Philhealth;
-import com.itseasyright.app.taxphcalculator.Entities.Sss;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 /**
  * Created by dione on 7 Sep 2016.
  */
 public class SyncDataActivity extends AppCompatActivity implements ITaxCalculator {
 
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_data);
+        progressBar = (ProgressBar)findViewById(R.id.progressbar);
         new ReadFileFromServerAsync(getApplicationContext(), this).execute();
 
         //sss query
@@ -46,8 +40,10 @@ public class SyncDataActivity extends AppCompatActivity implements ITaxCalculato
     @Override
     public void OnFinishLoadingData(boolean result) {
         if (result) {
+            progressBar.setProgress(100);
             Intent mainClass = new Intent(SyncDataActivity.this, MainActivity.class);
             startActivity(mainClass);
+            Toast.makeText(SyncDataActivity.this, "Data is up to date.", Toast.LENGTH_SHORT).show();
             finish();
         }
 
